c7e786332739c0c65e990acb3a38302ae7a6e1d5
emrahelis <emrahelis@gmail.com>
2024-10-12 10:41:13 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+Console.WriteLine("Lütfen değer giriniz");
+try{
+
+int deger = int.Parse(Console.ReadLine());
+Console.WriteLine(deger);
+
+
+
+}
+catch(Exception ex){
+
+// Catch bloğu içerisinde gelen tüm exceptionları, bir dosyaya yazalım
+
+
+    DateTime dateTime = DateTime.Now;
+    File.AppendAllText("/Users/emrahelis/Desktop/deneme.txt","\n"+dateTime.ToShortTimeString()+ "---"+ ex.Message);
+
+}
